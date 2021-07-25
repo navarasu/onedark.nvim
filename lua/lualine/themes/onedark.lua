683912ffaad44278f1b1e3b56228be968fafc4db
Nova <nova@francium.tech>
2021-07-26 02:18:42 +0530

    #11 Updated lualine as per latest config

--- lua/lualine/themes/onedark.lua
+++ lua/lualine/themes/onedark.lua
@@ -15,19 +15,16 @@ local one_dark = {
     inactive = {
         a = {fg = colors.gray, bg = colors.bg, gui = 'bold'},
         b = {fg = colors.gray, bg = colors.bg},
-        c = {fg = colors.bg, bg = colors.bg},
-        z = {fg = colors.gray, bg = colors.bg}
+        c = {fg = colors.gray, bg = c.bg1},
     },
     normal = {
         a = {fg = colors.bg, bg = colors.green, gui = 'bold'},
-        b = {fg = colors.fg, bg = colors.bg},
-        c = {fg = colors.fg, bg = colors.bg},
-        z = {fg = colors.fg, bg = colors.bg}
+        b = {fg = colors.fg, bg = c.bg3},
+        c = {fg = colors.fg, bg = c.bg1},
     },
     visual = {a = {fg = colors.bg, bg = colors.purple, gui = 'bold'}},
     replace = {a = {fg = colors.bg, bg = colors.red, gui = 'bold'}},
     insert = {a = {fg = colors.bg, bg = colors.blue, gui = 'bold'}},
     command = {a = {fg = colors.bg, bg = colors.yellow, gui = 'bold'}}
 }
-
-return one_dark
+return one_dark;

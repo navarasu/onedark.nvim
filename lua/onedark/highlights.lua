9d57ce7aae8473bd22d9e61c067f021a9e418c73
doot <gugegby@gmail.com>
2022-11-20 23:51:30 +0800

    fix indent blank line highlights order

--- lua/onedark/highlights.lua
+++ lua/onedark/highlights.lua
@@ -489,13 +489,13 @@ hl.plugins.ts_rainbow = {
 }
 
 hl.plugins.indent_blankline = {
-    IndentBlankLineIndent4 = colors.LightGrey,
     IndentBlankLineIndent1 = colors.Blue,
     IndentBlankLineIndent2 = colors.Green,
     IndentBlankLineIndent3 = colors.Cyan,
+    IndentBlankLineIndent4 = colors.LightGrey,
     IndentBlankLineIndent5 = colors.Purple,
     IndentBlankLineIndent6 = colors.Red,
-    IndentBlankLineContext = {fg = c.orange, bg = c.bg3, bold = true},
+    IndentBlankLineContext = { fg = c.orange, bg = c.bg3, bold = true },
 }
 
 hl.plugins.mini = {

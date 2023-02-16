navarasu/onedark.nvim

commit f24ecc14519b34055644353ba10438afd288bfb4

fix(plugin): indentBlankline hl group names
* Fix indentBlankline hl group names
* Add plugin missing hl groups
* Use grey for MiniIndentscopeSymbol

Fixes: 8a082b9153d1 ("fix(plugin): indentBlankline hl group names")

lua/onedark/highlights.lua
@@ -493,13 +493,17 @@ hl.plugins.ts_rainbow = {
 }
 
 hl.plugins.indent_blankline = {
-    IndentBlankLineIndent1 = colors.Blue,
-    IndentBlankLineIndent2 = colors.Green,
-    IndentBlankLineIndent3 = colors.Cyan,
-    IndentBlankLineIndent4 = colors.LightGrey,
-    IndentBlankLineIndent5 = colors.Purple,
-    IndentBlankLineIndent6 = colors.Red,
-    IndentBlankLineContext = { fg = c.orange, bg = c.bg3, bold = true },
+    IndentBlanklineIndent1 = colors.Blue,
+    IndentBlanklineIndent2 = colors.Green,
+    IndentBlanklineIndent3 = colors.Cyan,
+    IndentBlanklineIndent4 = colors.LightGrey,
+    IndentBlanklineIndent5 = colors.Purple,
+    IndentBlanklineIndent6 = colors.Red,
+    IndentBlanklineChar = { fg = c.bg1, gui = "nocombine" },
+    IndentBlanklineContext = { fg = c.orange, bg = c.bg3, bold = true },
+    IndentBlanklineContextChar = { fg = c.bg1, gui = "nocombine" },
+    IndentBlanklineContextStart = { sp = c.bg1, gui = "underline" },
+    IndentBlanklineContextSpaceChar = { gui = "nocombine" },
 }
 
 hl.plugins.mini = {
@@ -508,7 +512,7 @@ hl.plugins.mini = {
     MiniCursorword = { fmt = "underline" },
     MiniCursorwordCurrent = { fmt = "underline" },
 
-    MiniIndentscopeSymbol = { fg = c.light_grey },
+    MiniIndentscopeSymbol = { fg = c.grey },
     MiniIndentscopePrefix = { fmt = "nocombine" }, -- Make it invisible
 
     MiniJump = { fg = c.purple, fmt = "underline", sp = c.purple },

commit 9fbdf79f1fa7a8d61cdf147d3601da7681ffd6e3

fix(hi): indentblankline context highlights
Fixes: f24ecc14519b ("fix(plugin): indentBlankline hl group names")

lua/onedark/highlights.lua
@@ -500,9 +500,9 @@ hl.plugins.indent_blankline = {
     IndentBlanklineIndent5 = colors.Purple,
     IndentBlanklineIndent6 = colors.Red,
     IndentBlanklineChar = { fg = c.bg1, gui = "nocombine" },
-    IndentBlanklineContext = { fg = c.orange, bg = c.bg3, bold = true },
-    IndentBlanklineContextChar = { fg = c.bg1, gui = "nocombine" },
-    IndentBlanklineContextStart = { sp = c.bg1, gui = "underline" },
+    IndentBlanklineContext = { fg = c.orange, bg = c.grey, bold = true },
+    IndentBlanklineContextChar = { fg = c.grey, gui = "nocombine" },
+    IndentBlanklineContextStart = { sp = c.grey, gui = "underline" },
     IndentBlanklineContextSpaceChar = { gui = "nocombine" },
 }
 